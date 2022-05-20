7f9572e1c86aa4806fe7ad03405f8ef88b7106ec
Giacomo Pilotti <giacomo.pilotti@student.tugraz.at>
2022-05-20 14:08:52 +0200

    comments

--- main.py
+++ main.py
@@ -8,21 +8,40 @@
 
 from boxdynamics import BoxEnv
 
+# create an environment instance
+# if there is no "config.json" file
+# in the current working directory,
+# then the deafult configuration will
+# be used
 env = BoxEnv()
 
 # lets you create an environment by using the GUI
 env.world_design()
 
-# when you finish your design, you can save the
-# environment configuration with the name "myconf.json"
-# by uncommenting the following line
+# SAVE CONFIGURATION
+# save the environment configuration with the name
+# "myconf.json" by uncommenting the following line
+# this also includes the environment design
 # env.save_conf("myconf.json")
 
+# LOAD CONFIGURATION
 # if you already have a configuration named "myconf.json"
-# file with or without environment definition, you can
-# use it by uncommenting the followig line
-# this would overwrite the previous configuration
+# file you can use it by uncommenting the followig line
+# this overwrites every previous configuration
 # env.load_conf("myconf.json")
+
+# SAVE DESIGN
+# if you just want to save the design named "mydesign.json"
+# to utilize it later on, uncomment the following line
+# env.save_design("mydesign.json")
+
+# LOAD DESIGN
+# if you have a design named "mydesign.json"
+# you can load it by uncommenting the following line
+# env.load_design("mydesign.json")
+
+# note that you could also load and save designs
+# from from the GUI
 
 # simulation loop
 while True:
